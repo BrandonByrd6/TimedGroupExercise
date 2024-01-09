a6001bc6df66fb37b778dba754b0c62d1b9a64ff
Isaiah Schmidt <isiahdschmidt@outlook.com>
2024-01-09 18:32:37 -0500

    fixed overall small changes

--- TGE.Models/Comment/CommentCreate.cs
+++ TGE.Models/Comment/CommentCreate.cs
@@ -9,7 +9,7 @@ namespace TGE.Models.Comment
     public class CommentCreate
     {
         [Required]
-        public int PostId { get; set; };
+        public int PostId { get; set; }
 
         [Required]
         [MinLength(1, ErrorMessage = "{0} must be at least {1} character long.")]
